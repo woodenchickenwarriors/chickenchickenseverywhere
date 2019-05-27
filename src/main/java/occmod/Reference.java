fd61ce0079215c452b3020abfcfcc3a16958efff
shadowbq <shadowbq@gmail.com>
2019-05-27 14:51:51 -0400

    initial commit

---
+++ src/main/java/occmod/Reference.java
@@ -0,0 +1,12 @@
+package occmod;
+
+public class Reference {
+
+    public static final String MOD_ID = "occ";
+    public static final String NAME = "Chicken Mod";
+    public static final String VERSION = "1.0";
+    
+    public static final String CLIENT_PROXY = "occmod.proxies.ClientProxy";
+    public static final String COMMON_PROXY = "occmod.proxies.ServerProxy";
+    
+}
---
+++ src/main/java/occmod/handlers/RegistryHandler.java
@@ -0,0 +1,16 @@
+package occmod.handlers;
+
+import net.minecraft.item.Item;
+import net.minecraftforge.event.RegistryEvent;
+
+public class RegistryHandler {
+    
+    public static void Client() {
+
+    }
+    
+    public static void Common() {
+
+    }
+    
+}
