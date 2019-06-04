5fa1f37aaf1c0298b8e146fd37c91068458af124
shadowbq <shadowbq@gmail.com>
2019-06-04 09:41:10 -0400

    clean up filepath

--- build.bat
+++ build.bat
@@ -1,5 +1,7 @@
 @echo off
-call gradlew build
-move build\libs\modid-1.0.jar build\libs\occ-mod_v1.0_mc1.11.2.jar
+rem call gradlew build
+rem move build\libs\modid-1.0.jar build\libs\occ-mod_v1.0_mc1.11.2.jar
 echo "Download new Minecraft Mod 'occ-mod_v1.0_mc1.11.2.jar' from http://10.0.3.12:8100"
-sheret\sheret.exe -d build\libs\ -f sheret\host.log -p 8100
+cd logs
+..\sheret\sheret.exe -d ..\build\libs\ -f -p 8100
+cd ..
